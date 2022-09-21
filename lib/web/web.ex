991c031994ead6874e96759be0986a64e1626897
Mayel de Borniol <git@mayel.space>
2022-09-21 20:09:00 +1200

    v

--- lib/web/web.ex
+++ lib/web/web.ex
@@ -102,8 +102,8 @@ defmodule Bonfire.PublisherThesis.Web do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
-      # Import LiveView helpers (live_render, live_component, live_patch, etc)
-      import Phoenix.LiveView.Helpers
+      # Import component helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
